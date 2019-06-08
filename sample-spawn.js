9f51fc98b77ec3a0e04429542adf9f96c27e172d
dallcharm <dallcharm@gmail.com>
2019-06-07 20:01:05 -0500

    add exec and spawn

---
+++ sample-spawn.js
@@ -0,0 +1,11 @@
+const { spawn } = require('child_process');
+const result = spawn('git',['branch']);
+
+result.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+/*
+result.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });*/
